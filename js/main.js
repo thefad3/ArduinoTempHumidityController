ed673ad154db580bb806dfdffab7d2708726bfb9
Chris Lynch (vVv F R 0 O T x) <thefad3@fullsail.edu>
2015-07-20 03:37:51 +0000

    Fresh off the plane konichiwa bitches

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 $(document).ready(function(){
+
     var url ="https://data.sparkfun.com/output/wpDpgRyG5wfOQpQNJaaD.json";
+    
     $.get(url, function(data) {
+      
+            $(".temp").prepend(data[0].t);
+            $(".time").append(data[0].timestamp);
+            $(".humid").prepend(Math.round(data[0].h));
+            
         for(i=0; i<data.length; i++){
-            $(".result").append('<div>','<h5>Time: </h5>', data[i].timestamp,  ' <h5>Humidity: </h5>', data[i].h, '<h5> Temp: </h5>', data[i].t, '<hr width="100%"></div>');
+            var date = new Date(Date.parse(data[i].timestamp));
+            var d = new Date();
+            var time = new Date();
+
+            $(".result").append('<div class="col-md-6"><h3><span class="glyphicon glyphicon-chevron-right"></span>  '+d.toDateString(date)+'</h3><div class="blockOutline bg-primary"><p>'+'<blockquote><b>Time:</b> '+date.toTimeString()+'</blockquote><blockquote><b>Humidity:</b> '+Math.round(data[i].h)+ ' %</blockquote><blockquote> <b>Temp:</b> '+ data[i].t+ ' &#8457</blockquote></p></div></div>');
         }
     });
-
 });
