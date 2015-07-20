8923e1fd8f245e009920a90f00494d2ff0472828
Chris Lynch (vVv F R 0 O T x) <thefad3@fullsail.edu>
2015-07-20 20:50:27 +0000

    Fixing a bug yo.

--- js/main.js
+++ js/main.js
@@ -6,10 +6,10 @@ $(document).ready(function(){
             $(".time").append(data[0].timestamp);
             $(".humid").prepend(Math.round(data[0].h));
             
-        for(i=0; i<data.length; i++){
-            var date = new Date(Date.parse(data[i].timestamp));
+        for(i=1; i<data.length; i++){
+            var date = new Date(data[i].timestamp);
             var d = new Date();
-            $(".result").append('<div class="col-md-6"><h3><span class="glyphicon glyphicon-chevron-right"></span>  '+d.toDateString(date)+'</h3><div class="blockOutline bg-primary"><p>'+'<blockquote><b>Time:</b> '+date.toTimeString()+'</blockquote><blockquote><b>Humidity:</b> '+Math.round(data[i].h)+ ' %</blockquote><blockquote> <b>Temp:</b> '+ data[i].t+ ' &#8457</blockquote></p></div></div>');
+            $(".result").append('<div class="col-md-6"><h3><span class="glyphicon glyphicon-chevron-right"></span>  '+date.toDateString()+'</h3><div class="blockOutline bg-primary"><p>'+'<blockquote><b>Time:</b> '+date.toTimeString()+'</blockquote><blockquote><b>Humidity:</b> '+Math.round(data[i].h)+ ' %</blockquote><blockquote> <b>Temp:</b> '+ data[i].t+ ' &#8457</blockquote></p></div></div>');
         }
     });
 });
