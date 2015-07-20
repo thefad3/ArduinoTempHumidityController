9e5fec11238ee3a5f8dcfb0c64f04ac24b29765c
Chris Lynch (vVv F R 0 O T x) <thefad3@fullsail.edu>
2015-07-20 03:39:02 +0000

    The less code the butter

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 $(document).ready(function(){
-
     var url ="https://data.sparkfun.com/output/wpDpgRyG5wfOQpQNJaaD.json";
-    
     $.get(url, function(data) {
       
             $(".temp").prepend(data[0].t);
@@ -11,8 +9,6 @@ $(document).ready(function(){
         for(i=0; i<data.length; i++){
             var date = new Date(Date.parse(data[i].timestamp));
             var d = new Date();
-            var time = new Date();
-
             $(".result").append('<div class="col-md-6"><h3><span class="glyphicon glyphicon-chevron-right"></span>  '+d.toDateString(date)+'</h3><div class="blockOutline bg-primary"><p>'+'<blockquote><b>Time:</b> '+date.toTimeString()+'</blockquote><blockquote><b>Humidity:</b> '+Math.round(data[i].h)+ ' %</blockquote><blockquote> <b>Temp:</b> '+ data[i].t+ ' &#8457</blockquote></p></div></div>');
         }
     });
